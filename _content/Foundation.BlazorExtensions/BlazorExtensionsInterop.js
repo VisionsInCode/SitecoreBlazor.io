a286a0e0c89e3d58c99a36dbc814abb48d48a0b3
GoranHalvarsson <goranhalvarsson@gmail.com>
2018-05-13 20:07:32 +0200

    First
    First

---
+++ _content/Foundation.BlazorExtensions/BlazorExtensionsInterop.js
@@ -0,0 +1,20 @@
+﻿Blazor.registerFunction('Foundation.BlazorExtensions.BlazorExtensionsInterop.ShowRawHtml', (elementId, html) => {
+  let el = document.getElementById(elementId);
+  if (el) {
+    el.innerHTML = html;
+    return el.innerHTML;
+  }
+  else {
+    console.log("HTML not rendered");
+  }
+});
+
+Blazor.registerFunction('Foundation.BlazorExtensions.BlazorExtensionsInterop.CurrentUrl', ()=> {
+  let path = window.location.pathname;
+  if (path) {
+    return path;
+  }
+  else {
+    console.log("No current path");
+  }
+});
---
+++ interop.js
@@ -0,0 +1,14 @@
+﻿
+
+
+Blazor.registerFunction('MarkdownComponent.HtmlRendererInterop.RenderMarkdown', function (message) {
+
+  let el = document.getElementById("markdown-component");
+  if (el) { el.innerHTML = message; }
+  else {
+    console.log("HTML not rendered");
+  }
+  console.log(message);
+});
+
+
